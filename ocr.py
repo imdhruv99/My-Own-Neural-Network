de854891d5727531ac58afb606a440e471647c2d
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-05-09 14:41:44 +0530

    [Added] Sigmoid functions

--- ocr.py
+++ ocr.py
@@ -19,7 +19,7 @@ class OCRNeuralNetwork:
 
     def __init__(self, num_hidden_nodes, data_matrix, data_labels, training_indices, use_file=True):
         self.sigmoid = np.vectorize(self._sigmoid_scalar)
-        self.sigmoid_prime = np.vectorize(self.sigmoid_prime_scalar)
+        self.sigmoid_prime = np.vectorize(self._sigmoid_prime_scalar)
         self._use_file = use_file
         self.data_matrix = data_matrix
         self.data_labels = data_labels
@@ -39,3 +39,19 @@ class OCRNeuralNetwork:
         else:
 
             self._load()
+
+    
+    def _rand_initialize_weights(self, size_in, size_out):
+        return [((x * 0.12) - 0.06) for x in np.random.rand(size_out, size_in)]
+    
+    # The sigmoid activation function
+    def _sigmoid_scalar(self, z):
+        return 1 / (1 + math.e ** -z)
+    
+    def _sigmoid_prime_scalar(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+    
+    def _draw(self, sample):
+        pixelArray = [sample[j:j+self.WIDTH_IN_PIXEL] for j in range(0, len(sample), self.WIDTH_IN_PIXEL)]
+        plt.imshow(zip(*pixalArray), cmap = cm.Greys_r, interpolation='nearest')
+        plt.show()
